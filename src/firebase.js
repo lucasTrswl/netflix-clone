32e395ab6e52c716e8b7a8396a3eaa7cccb88162
lucasTrswl <lucasturschwell@hotmail.com>
2022-12-20 16:45:51 +0100

    initialisation du projet

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAhr_qPYP01rqSzUrPSKm201QOc-UYmx8g",
+    authDomain: "netflix-clone-17539.firebaseapp.com",
+    projectId: "netflix-clone-17539",
+    storageBucket: "netflix-clone-17539.appspot.com",
+    messagingSenderId: "351683948599",
+    appId: "1:351683948599:web:512e95c39aa58643454fb2"
+  };
